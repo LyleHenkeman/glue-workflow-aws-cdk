0ebdecb5164361eb2ed2ef6b29e247a39b173d50
Lyle Henkeman <lylehenkeman@asml.com>
2021-11-25 10:50:00 +0100

    Removing syth command

--- bin/blog-glue-workflow.ts
+++ bin/blog-glue-workflow.ts
@@ -6,8 +6,6 @@
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { BlogGlueWorkFlowStack } from '../lib/blog-glue-workflow-stack';
-import { RedshiftVpcStack } from '../lib/blog-redshift-vpc-stack';
-
 
 const app = new cdk.App();
 
@@ -15,11 +13,3 @@ const workflow_stack = new BlogGlueWorkFlowStack(app, 'workflow-stack', {
   stackName: 'workflow-stack',
   description: 'creates the Glue workflow, Crawlers, Jobs and triggers'
 });
-
-const redshift_vpc_stack = new RedshiftVpcStack(app, 'redshift-vpc-stack', {
-  glueRoleGrantSecretRead: workflow_stack.glueRole,
-  stackName: 'redshift-vpc-stack',
-  description: 'creates the VPC, Glue Connection, and Redshift cluster'
-})
-
-
